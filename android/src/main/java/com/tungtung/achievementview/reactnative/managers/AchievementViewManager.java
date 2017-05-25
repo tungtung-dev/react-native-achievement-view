5d5370b16c912efbf1e35f0a944a2f3cbdbcb3f6
Tien Nguyen <toantien.vn91@gmail.com>
2017-05-25 14:40:30 +0700

    refactor

--- android/src/main/java/com/tungtung/achievementview/reactnative/managers/AchievementViewManager.java
+++ android/src/main/java/com/tungtung/achievementview/reactnative/managers/AchievementViewManager.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 - 2017 Tien Nguyen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tungtung.achievementview.reactnative.managers;
 
 import android.util.Log;
@@ -6,10 +21,6 @@ import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.tungtung.achievementview.AchievementView;
-
-/**
- * This file is created by Tien Nguyen on 5/25/17.
- */
 
 public class AchievementViewManager extends SimpleViewManager<AchievementView> {
     private static final String NAME = "AchievementView";
--- android/src/main/java/com/tungtung/achievementview/reactnative/packages/AchievementViewPackage.java
+++ android/src/main/java/com/tungtung/achievementview/reactnative/packages/AchievementViewPackage.java
@@ -1,18 +1,30 @@
+/*
+ * Copyright 2017 - 2017 Tien Nguyen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tungtung.achievementview.reactnative.packages;
 
-import com.achievement_view.reactnative.managers.AchievementViewManager;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.tungtung.achievementview.reactnative.managers.AchievementViewManager;
 
 import java.util.Collections;
 import java.util.List;
 
-/**
- * This file is created by Tien Nguyen on 5/12/17.
- */
 
 public class AchievementViewPackage implements ReactPackage {
 
